b22723e08dcae20a4ebcba4b09bf3831ff24fba2
Matthieu Bauwens <mattbws93@gmail.com>
2023-11-21 15:51:18 +0100

    prog py bdd

---
+++ SalleConcert/recup_donnee_db.py
@@ -0,0 +1,48 @@
+import mysql.connector
+from mysql.connector import Error
+import matplotlib.pyplot as plt
+
+# Fonction pour se connecter à la base de données
+def connect():
+
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='salle_concert',
+            user='admin',
+            password='admin'
+        )
+
+        return connection
+
+
+
+# Fonction pour récupérer et afficher les données de la table "spectateurs"
+def fetch_and_display_data(connection):
+
+        cursor = connection.cursor()
+
+        # Exécutez votre requête ici
+        requete_spec = "SELECT * FROM spectateurs"
+        cursor.execute(requete_spec)
+
+        # Récupérez les résultats
+        rows = cursor.fetchall()
+
+        # Affichez les résultats
+        for row in rows:
+            print(row)
+
+        # Fermez le curseur et la connexion
+        cursor.close()
+
+
+# Programme principal
+def main():
+    connection = connect()
+
+    if connection:
+        fetch_and_display_data(connection)
+        connection.close()
+
+if __name__ == "__main__":
+    main()
