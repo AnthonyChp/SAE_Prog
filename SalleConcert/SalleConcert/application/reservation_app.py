0daf123855a412ec4d62208f64db0c77229660ab
AnthonyChap <chapus.anthony1002@gmail.com>
2023-12-07 10:19:43 +0100

    AJout des commentaires

--- SalleConcert/SalleConcert/application/reservation_app.py
+++ SalleConcert/SalleConcert/application/reservation_app.py
@@ -12,8 +12,13 @@ class ReservationApp(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        '''
+        Programme qui nous permet de faire tout l'affichage de base de notre application
+        '''
         self.setWindowTitle('Réservation de Concert')
-        self.showMaximized()  # Maximize the window
+        self.showMaximized()  # Met la fenêtre a la taille de l'écran
+
+        #--- Création de tous nos boutons ---#
 
         self.login_button = QPushButton('Se connecter')
         self.login_button.clicked.connect(self.show_login_dialog)
@@ -27,17 +32,20 @@ class ReservationApp(QWidget):
 
         self.username_label = QLabel()
 
+        #------------------------------------#
+
+        #--- Création de tous nos layout pour placer nos boutons ---#
+
         layout = QGridLayout()
 
         layout.addWidget(self.login_button, 1, 1, 1, 5, alignment=Qt.AlignTop | Qt.AlignLeft)
-
         layout.addWidget(self.create_account_button, 1, 6, 1, 5, alignment=Qt.AlignTop | Qt.AlignRight)
-
         layout.addWidget(self.manage_database_button, 1, 6, 1, 5, alignment=Qt.AlignTop | Qt.AlignRight)
-
         layout.addWidget(self.username_label, 3, 1, 1, 10)
 
         self.seat_buttons = []
+
+        #-----------------------------------------------------------#
 
         # Connexion à la base de donnée
         connection = mysql.connector.connect(
@@ -72,6 +80,11 @@ class ReservationApp(QWidget):
         self.show()
 
     def show_login_dialog(self):
+        '''
+        Permet de lancer la fonction lorsque le bouton 'se connecter' afin de récupérer l'utilisateur dans la base
+        de donnée. Ajout d'une vérification si c'est un admin qui est se connecte sinon cela remplace le bouton
+        se connecter en 'Déconnexion'
+        '''
         if self.login_button.text() == 'Se connecter':
             login_dialog = LoginDialog(self)
 
@@ -87,20 +100,31 @@ class ReservationApp(QWidget):
             self.logout()
 
     def show_admin_controls(self):
+        '''
+        Affichage de l'interface 'Admin' losqu'il se connecte
+        '''
         self.username_label.setText("Connecté en tant qu'Admin")
         self.login_button.setText('Déconnexion')
         self.create_account_button.setVisible(False)
         self.manage_database_button.setVisible(True)
 
     def manage_database(self):
+        '''
+        Lancer la gestion d'admin
+        '''
         subprocess.run(['python3', '/home/etudiant/Documents/SAE_Prog/SalleConcert/SalleConcert/gestion_graphique/main.py'])
 
     def launch_create_account_script(self):
-        # Lancer le script de création de compte
+        '''
+        Lancer le script de création de compte
+        '''
         subprocess.run(['python3', '/home/etudiant/Documents/SAE_Prog/SalleConcert/SalleConcert/application/creation_compte.py'])
 
 
     def logout(self):
+        '''
+        Permet la déconnexion d'un utilisateur lorsque ce derniers est connecté
+        '''
         self.username_label.clear()
         self.login_button.setText('Se connecter')
         self.create_account_button.setVisible(True)
