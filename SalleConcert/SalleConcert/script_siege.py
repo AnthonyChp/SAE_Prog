e2d35118a300151f26abfb2238daee85e2a5ed49
etudiant <etudiant@deb12-vdi-rt>
2023-12-07 10:19:10 +0100

    script_siege.py

--- SalleConcert/SalleConcert/script_siege.py
+++ SalleConcert/SalleConcert/script_siege.py
@@ -1,8 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QPushButton, QVBoxLayout, QLabel, QMessageBox
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import Qt
 import os 
+import mysql.connector
 
 
 class SeatWindow(QWidget):
@@ -11,7 +12,8 @@ class SeatWindow(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Cinéma')
+        self.setWindowTitle('Salle de concert')
+        self.selected_seats = []
         self.setGeometry(100, 100, 400, 400)
 
         grid_layout = QGridLayout()
@@ -25,9 +27,60 @@ class SeatWindow(QWidget):
                 icon = QIcon(pixmap)
                 seat_btn.setIcon(icon)
                 seat_btn.setIconSize(pixmap.size())
+                seat_btn.setCheckable(True)  
+                seat_btn.clicked.connect(self.on_seat_selected) 
                 grid_layout.addWidget(seat_btn, i, j)
 
+        validate_btn = QPushButton("Valider")
+        validate_btn.clicked.connect(self.save_selected_seats)  # Connecter le bouton Valider à la méthode pour enregistrer les sièges
+        grid_layout.addWidget(validate_btn, 6, 0, 1, 5)  # Ajouter le bouton Valider en bas
         self.show()
+
+    def on_seat_selected(self):
+        seat_btn = self.sender()  # Récupérer le bouton qui a déclenché le signal
+
+        if seat_btn.isChecked():  # Vérifier si le bouton est coché (sélectionné)
+            seat_btn.setStyleSheet("border: 2px solid green;")  # Changer le style du bouton
+            seat_name = seat_btn.text()
+            if seat_name not in self.selected_seats:
+                self.selected_seats.append(seat_name)  # Ajouter le siège à la liste des sièges sélectionnés
+        else:
+            seat_btn.setStyleSheet("")  # Réinitialiser le style du bouton
+            seat_name = seat_btn.text()
+            if seat_name in self.selected_seats:
+                self.selected_seats.remove(seat_name)  # Enlever le siège de la liste des sièges sélectionnés
+
+        print("Sièges sélectionnés :", self.selected_seats)  # Afficher les sièges sélectionnés dans la console
+
+    def save_selected_seats(self):
+        if not self.selected_seats:
+            QMessageBox.warning(self, "Avertissement", "Aucun siège sélectionné!")
+            return
+
+        try:
+            # Connexion à la base de données MySQL
+            connection = mysql.connector.connect(
+                host='localhost',
+                user='admin',
+                password='admin',
+                database='salle_concert'
+            )
+
+            cursor = connection.cursor()
+
+            # Insertion des sièges sélectionnés dans la table MySQL
+            for seat in self.selected_seats:
+                cursor.execute("INSERT INTO spectateurs (emplacement) VALUES (%s)", (seat,))
+            
+            connection.commit()
+            connection.close()
+
+            QMessageBox.information(self, "Information", "Sièges enregistrés avec succès!")
+
+        except mysql.connector.Error as e:
+            print("Erreur lors de l'enregistrement des sièges :", e)
+            QMessageBox.critical(self, "Erreur", "Erreur lors de l'enregistrement des sièges!")
+    
 
 
 def main():
