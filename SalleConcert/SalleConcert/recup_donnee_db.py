e2f009e7e34c3cdd18b08fca713641fb74ac7bf2
etudiant <etudiant@deb12-vdi-rt>
2023-12-12 09:19:04 +0100

    Documentation sphynx

--- SalleConcert/SalleConcert/recup_donnee_db.py
+++ SalleConcert/SalleConcert/recup_donnee_db.py
@@ -1,46 +1,72 @@
 import mysql.connector
+from mysql.connector.connection import MySQLConnection
 
-# Fonction pour se connecter à la base de données
-def connect():
+def connect() -> MySQLConnection:
+    """
+    Établit une connexion à la base de données MySQL.
 
-        connection = mysql.connector.connect(
-            host='localhost',
-            database='salle_concert',
-            user='admin',
-            password='admin'
-        )
+    Returns:
+        MySQLConnection: Un objet de connexion à la base de données MySQL.
+        
+    Raises:
+        mysql.connector.Error: En cas d'échec de la connexion à la base de données.
+    """
+    connection = mysql.connector.connect(
+        host='localhost',
+        database='salle_concert',
+        user='admin',
+        password='admin'
+    )
+    return connection
 
-        return connection
+def fetch_and_display_data(connection: MySQLConnection) -> None:
+    """
+    Récupère et affiche toutes les données de la table "spectateurs".
 
+    Args:
+        connection (MySQLConnection): Un objet de connexion à la base de données MySQL.
+        
+    Raises:
+        mysql.connector.Error: En cas d'erreur lors de l'exécution de la requête SQL.
+    """
+    cursor = connection.cursor()
 
+    # Exécutez votre requête SQL ici
+    requete_spec = "SELECT * FROM spectateurs"
+    cursor.execute(requete_spec)
 
-# Fonction pour récupérer et afficher les données de la table "spectateurs"
-def fetch_and_display_data(connection):
+    # Récupérez les résultats
+    rows = cursor.fetchall()
 
-        cursor = connection.cursor()
+    # Affichez les résultats
+    for row in rows:
+        print(row)
 
-        # Exécutez votre requête ici
-        requete_spec = "SELECT * FROM spectateurs"
-        cursor.execute(requete_spec)
+    # Fermez le curseur
+    cursor.close()
 
-        # Récupérez les résultats
-        rows = cursor.fetchall()
+def main() -> None:
+    """
+    Fonction principale pour démontrer la connexion à la base de données et la récupération des données.
+    
+    Raises:
+        mysql.connector.Error: En cas d'échec de la connexion ou de l'exécution de la requête SQL.
+    """
+    try:
+        # Établir la connexion à la base de données
+        connection = connect()
 
-        # Affichez les résultats
-        for row in rows:
-            print(row)
+        # Vérifier si la connexion a réussi
+        if connection:
+            # Récupérer et afficher les données de la table "spectateurs"
+            fetch_and_display_data(connection)
 
-        # Fermez le curseur et la connexion
-        cursor.close()
+            # Fermer la connexion
+            connection.close()
 
-
-# Programme principal
-def main():
-    connection = connect()
-
-    if connection:
-        fetch_and_display_data(connection)
-        connection.close()
+    except mysql.connector.Error as err:
+        print(f"Erreur : {err}")
+        # Gérer l'erreur (par exemple, en affichant un message d'erreur ou en enregistrant des journaux)
 
 if __name__ == "__main__":
     main()
